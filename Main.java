74af3ba6dcc654a007a185524155e0a302d47424
ChristusRegnat <90288748+ChristusRegnat@users.noreply.github.com>
2023-04-28 09:49:10 -0500

    Daniel Demmin
    Completed

--- Main.java
+++ Main.java
@@ -1,19 +1,36 @@
-import java.util.Scanner;
-class Main {
-{   
+import java.util.*;
+class Main {   
    public static void main(String[] args) 
    {  
       final int QUARTS_IN_GALLON = 4;
-      int quartsNeeded = 18;
+      String quartsNeededStr = "";
+      int quartsNeededInt = 18;
       int gallonsNeeded;
+      boolean flag = false;
       int extraQuartsNeeded;
       Scanner input = new Scanner(System.in);
       System.out.print("Enter quarts needed >> ");
-      quartsNeeded = input.nextInt();
-      gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
-      extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
-      System.out.println("A job that needs " + quartsNeeded +
-         " quarts requires " + gallonsNeeded + " gallons plus " +
-         extraQuartsNeeded + " quarts.");
+      quartsNeededStr = input.next();
+      while (flag == false)
+      {
+        try {
+            quartsNeededInt = Integer.parseInt(quartsNeededStr);
+            if (quartsNeededInt > 0)
+                {
+                    flag = true;
+                }
+            gallonsNeeded = quartsNeededInt / QUARTS_IN_GALLON;
+            extraQuartsNeeded = quartsNeededInt % QUARTS_IN_GALLON;
+            System.out.println("A job that needs " + quartsNeededInt +
+            " quarts requires " + gallonsNeeded + " gallons plus " +
+            extraQuartsNeeded + " quarts.");
+            }
+            catch (NumberFormatException e) {
+            System.out.println("Incorrect Data - Please type number: ");
+            quartsNeededStr = input.next();
+            }
+      }
+
+
    }
 }
